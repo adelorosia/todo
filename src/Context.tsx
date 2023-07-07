3140139a4f170d3ba0fd49baa429601441dae2e0
adelorosia <adel_orosia@yahoo.com>
2023-07-07 11:43:28 +0200

    appContext edited

--- src/Context.tsx
+++ src/Context.tsx
@@ -5,15 +5,8 @@ export interface ITask {
   name: string;
   done: boolean;
 }
+type Color = "bg-red-500" | "bg-pink-500" | "bg-blue-500" | "bg-green-500" | "bg-yellow-500" | "bg-purple-500";
 
-export enum colorList {
-  red = "red",
-  pink = "pink",
-  blue = "blue",
-  green = "green",
-  yellow = "yellow",
-  purple = "purple",
-}
 
 interface IApp {
   items: ITask[];
@@ -21,7 +14,7 @@ interface IApp {
   task: string;
   setTask: (task: string) => void;
   color: string;
-  setColor: React.Dispatch<React.SetStateAction<string>>;
+  setColor: React.Dispatch<React.SetStateAction<Color>>;
   isButtonActive:boolean
   setIsButtonActive: React.Dispatch<React.SetStateAction<boolean>>;
   changeColor:React.Dispatch<React.SetStateAction<any>>
@@ -36,37 +29,11 @@ export const AppContext = createContext<IApp>({} as IApp);
 export const AppContextProvider: React.FC<IAppProvider> = ({ children }) => {
   const [items, setItems] = useState<ITask[]>([]);
   const [task, setTask] = useState("");
-  const [color, setColor] = useState("bg-red-500");
+  const [color, setColor] = useState<Color>("bg-red-500");
   const [isButtonActive, setIsButtonActive] = useState(true);
-  const changeColor=(giveColor:colorList)=>{
-    switch(giveColor){
-      case colorList.pink :
-      setColor("bg-pink-500");
-      console.log(color)
-      break;
-      case colorList.red :
-      setColor("bg-red-500");
-      console.log(color)
-      break;
-      case colorList.green :
-      setColor("bg-green-500");
-      console.log(color)
-      break;
-      case colorList.purple :
-      setColor("bg-purple-500");
-      console.log(color)
-      break;
-      case colorList.blue :
-      setColor("bg-blue-500");
-      console.log(color)
-      break;
-      case colorList.yellow :
-      setColor("bg-yellow-500");
-      console.log(color)
-      break;
-      default:
-        break;
-    }
+  const changeColor=(getColor:Color)=>{
+    setColor(getColor)
+    console.log(color)
   }
   return (
     <AppContext.Provider
--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -5,12 +5,12 @@ const Color = () => {
     const {changeColor}=useContext(AppContext)
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 mb-12 ">
-      <span className="btn-color-style bg-RED" onClick={()=>{changeColor("red")}}>Red</span>
-      <span className="btn-color-style bg-blue-700" onClick={()=>{changeColor("blue")}}>Blue</span>
-      <span className="btn-color-style bg-YELLOW" onClick={()=>{changeColor("yellow")}}>Yellow</span>
-      <span className="btn-color-style bg-GREEN" onClick={()=>{changeColor("green")}}>Green</span>
-      <span className="btn-color-style bg-PURPLE" onClick={()=>{changeColor("purple")}}>Purple</span>
-      <span className="btn-color-style bg-PINK" onClick={()=>{changeColor("pink")}}>pink</span>
+      <span className="btn-color-style bg-RED" onClick={()=>{changeColor("bg-red-500")}}>Red</span>
+      <span className="btn-color-style bg-blue-700" onClick={()=>{changeColor("bg-blue-500")}}>Blue</span>
+      <span className="btn-color-style bg-YELLOW" onClick={()=>{changeColor("bg-yellow-500")}}>Yellow</span>
+      <span className="btn-color-style bg-GREEN" onClick={()=>{changeColor("bg-green-500")}}>Green</span>
+      <span className="btn-color-style bg-PURPLE" onClick={()=>{changeColor("bg-purple-500")}}>Purple</span>
+      <span className="btn-color-style bg-PINK" onClick={()=>{changeColor("bg-pink-500")}}>pink</span>
     </div>
   )
 }
